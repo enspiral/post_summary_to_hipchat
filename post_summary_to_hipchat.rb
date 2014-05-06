47748795a8dd1610fb1b8ff4012a0229827a3e27
ayumi5 <udakaayumi@ayumi.local>
2014-05-07 10:36:05 +1200

    change indent from tab to space2

--- post_summary_to_hipchat.rb
+++ post_summary_to_hipchat.rb
@@ -15,87 +15,85 @@ pass = "minute4649"
 
 class Minutedock
 
-	attr_reader :user, :pass
+  attr_reader :user, :pass
 
-	def initialize(args)
-		@user = args[:user]
-		@pass = args[:pass]
-	end
+  def initialize(args)
+    @user = args[:user]
+    @pass = args[:pass]
+  end
 
+  def get_collective_data(url)
+    result = open(url, :http_basic_authentication => [user, pass]).read
+    json = JSON.parser.new(result)
+      hash = json.parse()
+  end
 
-	def get_collective_data(url)
-		result = open(url, :http_basic_authentication => [user, pass]).read
-		json = JSON.parser.new(result)
-		hash = json.parse()
-	end
+  def get_summary(url = nil, hash, id_name, category)
+    if url
+      item = get_item_by_url(hash, url, id_name)
+    else
+      item = get_item(hash, id_name)
+      item = change_second_to_hour(item) if id_name == "duration"
+  end
 
-	def get_summary(url = nil, hash, id_name, category)
-		if url
-			item = get_item_by_url(hash, url, id_name)
-		else
-			item = get_item(hash, id_name)
-			item = change_second_to_hour(item) if id_name == "duration"
-		end
+  summary = make_statement(item, category)
+  end
 
-		summary = make_statement(item, category)
-	end
+  def put_together(contact, project, task, time, desciption)
+    length = contact.length
 
-	def put_together(contact, project, task, time, desciption)
-		length = contact.length
+    summaries = []
+      length.times{ |i|
+        summaries << contact[i] + "  " + project[i] + "  " + task[i] + "  " + time[i] + "  " + desciption[i]
+    }
+    return summaries
+  end
 
-		summaries = []
-		length.times{ |i|
-			summaries << contact[i] + "  " + project[i] + "  " + task[i] + "  " + time[i] + "  " + desciption[i]
-		}
-		return summaries
-	end
+  private
 
-	private
+  def get_name_by_id(hash, id)
+    hash.reject! { |data| return data["name"] if data["id"] == id }
+  end
 
-	def get_name_by_id(hash, id)
-		hash.reject! { |data| return data["name"] if data["id"] == id }
-	end
+  def get_id_from_entrydata(entrydata, id_name)
+    id = entrydata[id_name]
+  end
 
-	def get_id_from_entrydata(entrydata, id_name)
-		id = entrydata[id_name]
-	end
+  def get_item_by_url(hash, url, id_name)
+    hash.map { |data|
+      id = get_id_from_entrydata(data, id_name)
+      info = get_collective_data(url)
+      if id.class == Array
+        item = id.map{ |i| get_name_by_id(info, i)}.join(",")
+      else
+        item = get_name_by_id(info, id)
+      end
+    }
 
-	def get_item_by_url(hash, url, id_name)
-		hash.map { |data|
-			id = get_id_from_entrydata(data, id_name)
-			info = get_collective_data(url)
+  end
 
-			if id.class == Array
-				item = id.map{ |i| get_name_by_id(info, i)}.join(",")
-			else
-				item = get_name_by_id(info, id)
-			end
-		}
+  def get_item(hash, id_name)
+    hash.map { |data|
+      item = data[id_name]
+    }
+  end
 
-	end
+  def change_second_to_hour(times)
+    times.map { |time|
+      time = time / 60 / 60.0
+      time = "#{time.round(1)}h"
+    }
+  end
 
-	def get_item(hash, id_name)
-		hash.map { |data|
-			item = data[id_name]
-		}
-	end
-
-	def change_second_to_hour(times)
-		times.map { |time|
-			time = time / 60 / 60.0
-			time = "#{time.round(1)}h"
-		}
-	end
-
-	def make_statement(results, category)
-		results.map { |result|
-			if result.nil? || result.empty?
-				result = "No " + category
-			else
-				result = category + SEPARATOR + result
-			end
-		}
-	end
+  def make_statement(results, category)
+    results.map { |result|
+      if result.nil? || result.empty?
+        result = "No " + category
+      else
+        result = category + SEPARATOR + result
+      end
+    }
+  end
 
 end
 
